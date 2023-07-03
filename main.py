3f61a1f1cdf6a5cbb46cf4686a06232623e25706
Solo <songlong.thu@gmail.com>
2023-07-03 15:20:21 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort
+        return True
+
+
+if __name__ == '__main__':
+    x = Solution()
+    t = x.isPalindrome("A man, a plan, a canal: Panama")
+    # a=[]
+    # l = len(a)
+    # p= range(l)
+    # print (x.removeElement([],7))
+    print("\110\145\154\154\157\40\127\157\162\154\144\41")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
